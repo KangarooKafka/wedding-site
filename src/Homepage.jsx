5de5dc966969e574abb458785ff904d21c622091
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-04 12:54:35 -0500

    Created homepage

---
+++ src/Homepage.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Navbar />
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Homepage;
