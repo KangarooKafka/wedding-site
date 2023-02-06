7fbe76d153d0fb69a323b78f19a8580b918a851d
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-06 08:38:51 -0500

    Pages created for Info, Location, RSVP, and Share

---
+++ src/Location.jsx
@@ -0,0 +1,11 @@
+const Location = () => {
+    return (
+        <div className="location">
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <h1>Location page</h1>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </div>
+    )
+}
+
+export default Location;
---
+++ src/Share.jsx
@@ -0,0 +1,11 @@
+const Share = () => {
+    return (
+        <div className="share">
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <h1>Share page</h1>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </div>
+    )
+}
+
+export default Share;
