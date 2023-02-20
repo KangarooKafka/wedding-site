c8911f6304d10527226b3c34afb3064377da8168
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-20 08:19:27 -0500

    Replaced the Latin

--- src/Share.jsx
+++ src/Share.jsx
@@ -1,10 +1,15 @@
 const Share = () => {
     return (
-        <div className="share">
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <h1>Share page</h1>
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        </div>
+        <article className="share">
+            <h1>Share Pictures</h1>
+            <section>
+                <p>Ever go to a wedding, take some great pictures, then think to yourself, "oh I should send these to
+                [insert couple here] later", then never go through with it because it's a pain?</p>
+                <p>Well, this is still a pain, but I'm endeavoring to make it at least a little easier!</p>
+                <p>There's no shared folders to be invited to, no clouds or drives, just click the button below,
+                enter your social security number (kidding), and select the photos you'd like to share.</p>
+            </section>
+        </article>
     )
 }
 
