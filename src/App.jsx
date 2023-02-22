dd9a1c140501672320805f2ed7d22a56447af25a
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-22 10:26:16 -0500

    Updates to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import './style.css';
 import Navbar from "./Navbar";
-import Welcome from "./Welcome";
+import Home from "./Home";
 import Footer from "./Footer";
-import Homepage from "./Homepage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import RSVP from "./RSVP";
+import Info from "./Info";
+import Location from "./Location";
+import Share from "./Share";
+import Login from "./Login";
+import useGuest from "./useGuest";
 
 function App() {
-  return (
-      <Homepage/>
+    const {username, setUsername} = useGuest();
+
+    if(!username) {
+        return <Login setUser={setUsername} />
+    }
+    return (
+      <BrowserRouter>
+          <div className="background">
+            <header>
+                <Navbar />
+            </header>
+            <body>
+            <Routes>
+                <Route path={'/RSVP'} element={<RSVP />} />
+                <Route path={'/'} element={<Home />} />
+                <Route path={'/Location'} element={<Location />} />
+                <Route path={'/Share'} element={<Share />} />
+                <Route path={'/Info'} element={<Info />} />
+            </Routes>
+            </body>
+            <footer>
+                <Footer />
+            </footer>
+          </div>
+      </BrowserRouter>
+
+
+
   );
 }
 
