7f16f11940204b0ee98043c857974dc067d349a4
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-27 10:43:50 -0500

    Updates to RSVP

--- src/RSVP.jsx
+++ src/RSVP.jsx
@@ -1,11 +1,24 @@
-import {useReducer, useState} from "react";
+import { useState } from "react";
 
-const formReducer = (state, e) => {
-    console.log(e)
-    return {
+const formData = {
+    p1Coming: "",
+    p1Email: "",
+    p2Coming: "",
+    p2Email: "",
+    chAttending: "",
+    villaInt: "",
+    roommates: "",
+    foodRestricts: "",
+    notes: "",
+    rsvped: "true"
+}
 
-        [e.name]: e.value
-    }
+async function updateGuest(id, form) {
+    return fetch(`http://localhost:4000/guest/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(form)
+    })
 }
 
 const RSVP = () => {
@@ -13,110 +26,143 @@ const RSVP = () => {
     const personTwo = localStorage.getItem('p2FirstName');
     const children = localStorage.getItem('children');
 
-    const [formData, setFormData] = useReducer(formReducer,{
-        pOneAttending: "",
-        pTwoAttending: "",
-    });
-    const [attending, setAttending] = useState(false);
+    const [pOneAttend, setPOneAttend] = useState("");
+    const [pTwoAttend, setPTwoAttend] = useState("");
+    const [villaAttend, setVillaAttend] = useState("");
+    const [rsvped, setRsvped] = useState(localStorage.getItem('rsvped'));
 
     const handleChange = (e) => {
-        setFormData({
-            name: e.target.name,
-            value: e.target.value
-        });
+        switch (e.target.name) {
+            case 'pOneAttending':
+                formData.p1Coming = e.target.value;
+                setPOneAttend(e.target.value);
+                break;
+            case 'pTwoAttending':
+                formData.p2Coming = e.target.value;
+                setPTwoAttend(e.target.value);
+                break;
+            case 'villaAttending':
+                formData.villaInt = e.target.value;
+                setVillaAttend(e.target.value)
+                break;
+            case 'pOneEmail':
+                formData.p1Email = e.target.value;
+                break;
+            case 'pTwoEmail':
+                formData.p2Email = e.target.value;
+                break;
+            case 'chAttending':
+                formData.chAttending = e.target.value;
+                break;
+            case 'sharing':
+                formData.roommates = e.target.value;
+                break;
+            case 'food':
+                formData.foodRestricts = e.target.value;
+                break;
+            case 'notes':
+                formData.notes = e.target.value;
+                break;
+        }
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(formData)
+        localStorage.setItem('rsvped', 'true')
+        setRsvped('true')
+        await updateGuest(localStorage.getItem('userID'), formData)
     }
 
     return (
         <article className="RSVP">
             <h1>RSVP page</h1>
             <section>
-                <form onSubmit={handleSubmit}>
-                    <fieldset>
-                        <label>
-                            <p>Will {personOne} be attending?</p>
-                            <select
-                                name={'pOneAttending'}
-                                onChange={handleChange}
-                                value={formData.pOneAttending}
-                            >
-                                <option value={""}></option>
-                                <option value={"true"}>Yes</option>
-                                <option value={"false"}>No</option>
-                            </select>
-                        </label>
-                        {formData.pOneAttending === 'true' &&
+                {rsvped === 'true' &&
+                <div className={'rsvped-message'}>
+                    <p>
+                        Thank you for RSVPing!
+                    </p>
+                    <p>
+                        If you need to make a change, please <a href={'mailto:kevindarke@gmail.com'}>contact us directly</a>.
+                    </p>
+                </div>
+                }
+                {rsvped !== 'true' &&
+                    <form onSubmit={handleSubmit} onChange={handleChange}>
+                        <fieldset>
                             <label>
-                                <p>What is the best email address for {personOne}?</p>
-                                <input/>
-                            </label>
-                        }
-                        {personTwo.length > 0 &&
-                            <label>
-                                <p>Will {personTwo} be attending?</p>
-                                <select
-                                    name={'pTwoAttending'}
-                                    onChange={handleChange}
-                                    value={formData.pTwoAttending || ''}
-                                >
+                                <p>Will {personOne} be attending?</p>
+                                <select name={'pOneAttending'}>
                                     <option value={""}></option>
                                     <option value={"true"}>Yes</option>
                                     <option value={"false"}>No</option>
                                 </select>
                             </label>
-                        }
-                        {formData.pTwoAttending === 'true' &&
+                            {pOneAttend === 'true' &&
+                                <label>
+                                    <p>What is the best email address for {personOne}?</p>
+                                    <input name={'pOneEmail'}/>
+                                </label>
+                            }
+                            {personTwo.length > 0 &&
+                                <label>
+                                    <p>Will {personTwo} be attending?</p>
+                                    <select name={'pTwoAttending'}>
+                                        <option value={""}></option>
+                                        <option value={"true"}>Yes</option>
+                                        <option value={"false"}>No</option>
+                                    </select>
+                                </label>
+                            }
+                            {pTwoAttend === 'true' &&
+                                <label>
+                                    <p>What is the best email address for {personTwo}?</p>
+                                    <input name={'pTwoEmail'}/>
+                                </label>
+                            }
+                            {(pOneAttend === 'true' || pTwoAttend === 'true') && children.length > 0 &&
+                                <label>
+                                    <p>Will {children} be attending?</p>
+                                    <select name={'chAttending'}>
+                                        <option value={""}></option>
+                                        <option value={"true"}>Yes</option>
+                                        <option value={"false"}>No</option>
+                                    </select>
+                                </label>
+                            }
+                            {(pOneAttend === 'true' || pTwoAttend === 'true') &&
+                                <label>
+                                    <p>Would you be interested in staying at the Villa?</p>
+                                    <select name={'villaAttending'}>
+                                        <option value={""}></option>
+                                        <option value={"true"}>Yes</option>
+                                        <option value={"false"}>No</option>
+                                    </select>
+                                </label>
+                            }
+                            {(pOneAttend === 'true' || pTwoAttend === 'true') && villaAttend === 'true' &&
+                                <label>
+                                    <p>Is there anyone you think is going who you would be okay sharing a multi-bed room
+                                        with?</p>
+                                    <textarea name={'sharing'}/>
+                                </label>
+                            }
+                            {(pOneAttend === 'true' || pTwoAttend === 'true') &&
+                                <label>
+                                    <p>Do you have any food restrictions?</p>
+                                    <textarea name={'food'}/>
+                                </label>
+                            }
                             <label>
-                                <p>What is the best email address for {personTwo}?</p>
-                                <input/>
+                                <p>Are there any additional notes or messages you would like to add?</p>
+                                <textarea name={'notes'}/>
                             </label>
-                        }
-                        {attending && children.length > 0 &&
-                            <label>
-                                <p>Will {children} be attending?</p>
-                                <select>
-                                    <option value={""}></option>
-                                    <option value={"true"}>Yes</option>
-                                    <option value={"false"}>No</option>
-                                </select>
-                            </label>
-                        }
-                        {attending &&
-                            <label>
-                                <p>Would you be interested in staying at the Villa?</p>
-                                <select>
-                                    <option value={""}></option>
-                                    <option value={"true"}>Yes</option>
-                                    <option value={"false"}>No</option>
-                                </select>
-                            </label>
-                        }
-                        {attending &&
-                            <label>
-                                <p>Is there anyone you think is going who you would be okay sharing a multi-bed room
-                                    with?</p>
-                                <textarea/>
-                            </label>
-                        }
-                        {attending &&
-                            <label>
-                                <p>Do you have any food restrictions?</p>
-                                <textarea/>
-                            </label>
-                        }
-                        <label>
-                            <p>Are there any additional notes or messages you would like to add?</p>
-                            <textarea/>
-                        </label>
-                    </fieldset>
-                    <button type={"submit"}>
-                        Submit RSVP
-                    </button>
-                </form>
+                        </fieldset>
+                        <button type={"submit"}>
+                            Submit RSVP
+                        </button>
+                    </form>
+                }
             </section>
         </article>
     )
