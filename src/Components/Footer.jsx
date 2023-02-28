98bb9152427b822664120358842f8a12a6f58e95
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-28 14:12:54 -0500

    leaned up code and added comments

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+    return (
+        <footer className={"custom-footer"}>
+            <h3>Bride and Groom Contact:</h3>
+            <ul>
+                <li>
+                    <h4>Bailey James</h4>
+                    <p><a href="tel:2402053017">240-205-3017</a><br /></p>
+                    <p><a href="mailto:baileykandare@gmail.com">baileykandare@gmail.com</a></p>
+                </li>
+                <li>
+                    <h4>Kevin Darke</h4>
+                    <p><a href="tel:8108450806">810-845-0806</a><br /></p>
+                    <p><a href="mailto:kevindarke@gmail.com">kevindarke@gmail.com</a></p>
+                </li>
+            </ul>
+            <address>
+                <h5>Home address:</h5>
+                <p><a href="https://www.usps.com/ship/letters.htm">
+                    7714 Argonaut St<br />
+                    Severn, MD 21144</a></p>
+            </address>
+        </footer>
+    )
+}
