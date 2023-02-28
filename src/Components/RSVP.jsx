2d91fb801bcc6a3c30442619a19cfed63781c6f5
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-28 14:18:47 -0500

    Cleaned up code and added comments

--- src/Components/RSVP.jsx
+++ src/Components/RSVP.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+// Blank object for form data
 const formData = {
     p1Coming: "",
     p1Email: "",
@@ -13,6 +14,7 @@ const formData = {
     rsvped: "true"
 }
 
+// Makes API request to update the guest data
 async function updateGuest(id, form) {
     return fetch(`http://localhost:4000/guest/${id}`, {
         method: 'PUT',
@@ -21,16 +23,20 @@ async function updateGuest(id, form) {
     })
 }
 
-const RSVP = () => {
+export default function RSVP() {
+    // Get Guest data to customize form
     const personOne = localStorage.getItem('p1FirstName');
     const personTwo = localStorage.getItem('p2FirstName');
     const children = localStorage.getItem('children');
 
+    // Hooks for data that will change the form
     const [pOneAttend, setPOneAttend] = useState("");
     const [pTwoAttend, setPTwoAttend] = useState("");
     const [villaAttend, setVillaAttend] = useState("");
     const [rsvped, setRsvped] = useState(localStorage.getItem('rsvped'));
+    const [finished, setFinished] = useState(false)
 
+    // Delegates changes to form
     const handleChange = (e) => {
         switch (e.target.name) {
             case 'pOneAttending':
@@ -63,13 +69,30 @@ const RSVP = () => {
             case 'notes':
                 formData.notes = e.target.value;
                 break;
+            default:
+                break;
+        }
+
+        // Check is minimum requirements are met to allow RSVP submission
+        if ((formData.p1Coming.length > 0 && personTwo.length === 0) ||
+            (formData.p1Coming.length > 0 && formData.p2Coming.length > 0)) {
+            setFinished(true)
+        } else {
+            setFinished(false)
         }
     }
 
+    // Handles form submit
     const handleSubmit = async (e) => {
         e.preventDefault()
+
+        // Update local storage to show they've RSVPed
         localStorage.setItem('rsvped', 'true')
+
+        // Set RSVPed state variable to remove form
         setRsvped('true')
+
+        // Send the updated form to the API
         await updateGuest(localStorage.getItem('userID'), formData)
     }
 
@@ -77,17 +100,13 @@ const RSVP = () => {
         <article className="RSVP">
             <h1>RSVP page</h1>
             <section>
-                {rsvped === 'true' &&
+                {/* Display message or form, depending on if the guest has RSVPed already */}
+                {rsvped === 'true' ? (
                 <div className={'rsvped-message'}>
-                    <p>
-                        Thank you for RSVPing!
-                    </p>
-                    <p>
-                        If you need to make a change, please <a href={'mailto:kevindarke@gmail.com'}>contact us directly</a>.
-                    </p>
+                    <p>Thank you for RSVPing!</p>
+                    <p>If you need to make a change, please <a href={'mailto:kevindarke@gmail.com'}>contact us directly</a>.</p>
                 </div>
-                }
-                {rsvped !== 'true' &&
+                ) : (
                     <form onSubmit={handleSubmit} onChange={handleChange}>
                         <fieldset>
                             <label>
@@ -158,14 +177,12 @@ const RSVP = () => {
                                 <textarea name={'notes'}/>
                             </label>
                         </fieldset>
-                        <button type={"submit"}>
+                        <button type={"submit"} disabled={!finished}>
                             Submit RSVP
                         </button>
                     </form>
-                }
+                )}
             </section>
         </article>
     )
 }
-
-export default RSVP;
